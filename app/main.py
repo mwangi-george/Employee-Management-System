1078000332cb433615eef967f4ff00d95e39d97c
George Mwangi <mwangigeorge648@gmail.com>
2024-08-10 13:28:07 +0300

    Added employee router to main application

--- app/main.py
+++ app/main.py
@@ -1,11 +1,20 @@
 # Main Application entry point - George Mwangi
 
 from fastapi import FastAPI
+from app.routes import employee_routes
 
 
 def create_app() -> FastAPI:
     """ Create FastAPI application """
+
+    # instantiating the FastAPI class
     server = FastAPI()
+
+    # instantiate employee router
+    employee_router = employee_routes()
+
+    # include employee routes to main application
+    server.include_router(employee_router)
 
     return server
 
